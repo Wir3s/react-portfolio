3119063f90e57ef244e2bf57685b73aab3395680
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-03-19 11:47:33 -0500

    Building components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <h3>Copyright 2023 Wires International</h3>
+        </footer>
+    );
+}
+
+export default Footer;
