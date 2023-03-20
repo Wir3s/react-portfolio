d8c3636b5a9806ab132c1a844338704e05b950cf
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-03-20 05:58:58 -0500

    testing pages deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <h3>Copyright 2023 Wires International</h3>
+            <h5>Copyright 2023 Wires International</h5>
         </footer>
     );
 }
