3ee0eaac99954bf9f6930e1921cc75eedd1c4168
Wir3s <aandrebess@gmail.com>
2023-03-26 11:21:14 -0500

    Animation added to text

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,22 +6,35 @@ import Image from "react-bootstrap/Image";
 // import robImg from "../assets/images/robo.png";
 import dreImg from "../assets/images/andreabout.png";
 import "../styles/About.css";
+import styled, { keyframes } from "styled-components";
+import { fadeIn } from "react-animations";
+
+const FadeA = styled.div`
+  animation: 8s ${keyframes`${fadeIn}`};
+`;
+const FadeB = styled.div`
+  animation: 12s ${keyframes`${fadeIn}`};
+`;
 
 export default function About() {
   return (
     <Container fluid>
       <Row>
         <Col md="6" className="txtcol">
-                    
-           <h2> Full-Stack Web Developer</h2><br /> with a background in marketing.
-            Passionate about problem-solving while supporting a team. Skills
-            include Javascript, React, Express, jQuery, Node, and Bootstrap
-            combined with diverse marketing experience focused on communication
-            and the customer journey. 
-         
+          <FadeA>
+            <h2> Full-Stack Web Developer</h2>
+          </FadeA>
+          <br />
+          <FadeB>
+            {" "}
+            with a background in marketing. Passionate about problem-solving
+            while supporting a team. Skills include Javascript, React, Express,
+            jQuery, Node, and Bootstrap combined with diverse marketing
+            experience focused on communication and the customer journey.
+          </FadeB>
         </Col>
         <Col md="6">
-        <Image src={dreImg} width="200px" className="bio-img"  />
+          <Image src={dreImg} width="200px" className="bio-img" />
           {/* <Image src={robImg} width="260px" className="robo-img" /> */}
         </Col>
       </Row>
