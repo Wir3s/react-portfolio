74ada285f50cc4a8650ae8f4c6ef85badc1dbec6
Wir3s <aandrebess@gmail.com>
2023-03-26 19:23:05 -0500

    Final styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import dreImg from "../assets/images/andreabout.png";
+import dreImg2 from "../assets/images/andre_bio_bw.png";
 import "../styles/About.css";
 import styled, { keyframes } from "styled-components";
 import { fadeIn } from "react-animations";
@@ -21,20 +21,18 @@ export default function About() {
       <Row>
         <Col md="6" className="txtcol">
           <FadeA>
-            <h2> Full-Stack Web Developer</h2>
+            <h1> Full-Stack Web Developer</h1>
           </FadeA>
           <br />
           <FadeB>
-            {" "}
-            with a background in marketing. Passionate about problem-solving
+            <p>with a background in marketing. Passionate about problem-solving
             while supporting a team. Skills include Javascript, React, Express,
             jQuery, Node, and Bootstrap combined with diverse marketing
-            experience focused on communication and the customer journey.
+            experience focused on communication and the customer journey.</p>
           </FadeB>
         </Col>
         <Col md="6">
-          <Image src={dreImg} width="200px" className="bio-img" />
-          {/* <Image src={robImg} width="260px" className="robo-img" /> */}
+          <Image fluid src={dreImg2} width="400px" className="bio-img" />
         </Col>
       </Row>
     </Container>
