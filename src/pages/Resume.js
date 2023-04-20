3cf4ec56f7620f49404cc85fea7a4208d7c83206
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-04-20 06:13:54 -0500

    Removed bullets from list

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import myResume from '../assets/andre_bessette_resume2.docx';
+import React from "react";
+import myResume from "../assets/andre_bessette_resume2.docx";
 
 const styles = {
-    divStyle: {
-      height: '400px',
-    },
-  };
+  divStyle: {
+    height: "400px",
+    listStyleType: "none",
+  },
+  ul: {
+    listStyleType: "none",
+  },
+};
 
 export default function Resume() {
-    return (
-        <div style={styles.divStyle}>
-            <h1>Resume</h1>
-            <p>
-           <a href={myResume} download="andre_bessette_resume2.docx">Click here to download my resume.</a>
-            </p>
-            <h2>Proficiencies:</h2>
-            <ul>
-              <li>React</li>
-              <li>MongoDB</li>
-              <li>GraphQL</li>
-              <li>NodeJS</li>
-              <li>RESTful API</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div style={styles.divStyle}>
+      <h1>Resume</h1>
+      <p>
+        <a href={myResume} download="andre_bessette_resume2.docx">
+          Click here to download my resume.
+        </a>
+      </p>
+      <h2>Proficiencies:</h2>
+      <ul style={styles.ul}>
+        <li>React</li>
+        <li>MongoDB</li>
+        <li>GraphQL</li>
+        <li>NodeJS</li>
+        <li>RESTful API</li>
+      </ul>
+    </div>
+  );
 }
